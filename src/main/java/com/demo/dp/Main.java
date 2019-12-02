28f6e68ff9d93d1d7d0f909e3eb081b593a33a75
zhuhui132 <1321537109@qq.com>
2019-12-02 23:02:34 +0800

    2019-12-2

---
+++ src/main/java/com/demo/dp/Main.java
@@ -0,0 +1,11 @@
+package com.demo.dp;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i :arr){
+            System.out.println(i);
+        }
+
+    }
+}

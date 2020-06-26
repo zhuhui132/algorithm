428482b128991d30e718ac0d6529a5166cea4071
e <1321537109@qq.com>
2020-06-26 19:10:40 +0800

    add case

---
+++ src/main/java/com/algorithm/leeetcode_cn/CanConstruct.java
@@ -0,0 +1,11 @@
+package com.algorithm.leeetcode_cn;
+
+public class CanConstruct {
+    public static void main(String[] args) {
+
+    }
+//    public int canConstruct(String str ,int k){
+//
+//    }
+
+}
